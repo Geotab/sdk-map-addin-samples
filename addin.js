0ed8ddbd6e7b4fd4b2882715dba73e59d8582c5f
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2019-08-09 00:33:17 -0700

    Docs: Comment Out Console Logs

--- addin.js
+++ addin.js
@@ -4,7 +4,7 @@ geotab.addin.request = (elt, service) => {
     var currUser;
     var isUIMetric;
     service.api.getSession().then(sessionInfo => {
-        console.log("SESSION:", sessionInfo);
+        // console.log("SESSION:", sessionInfo);
         currUser = sessionInfo.userName;
 
         service.api.call("Get", {
@@ -13,20 +13,20 @@ geotab.addin.request = (elt, service) => {
                 name: currUser
             }
         }).then(user => {
-            console.log("USER", user);
+            // console.log("USER", user);
             isUIMetric = user[0].isMetric;
         });
     });
 
     // Subscribe to any mouseover events
     service.events.attach('over', data => {
-        console.log(data);
-        console.log("Type: ", data.type);
+        // console.log(data);
+        // console.log("Type: ", data.type);
         if (data.type == "trip") {
             var tripId = data.entity.id;
             var deviceId = data.entity.device.id;
             var hoverTimestamp = data.entity.dateTime;
-            console.log(tripId, deviceId, hoverTimestamp);
+            // console.log(tripId, deviceId, hoverTimestamp);
 
             service.api.multiCall([
                 ["Get", {
@@ -60,7 +60,7 @@ geotab.addin.request = (elt, service) => {
 
                 var odometerValue = isUIMetric ? `${Math.round(rawOdo / 1000)} km` : `${Math.round(rawOdo / 1609.344)} mi`;
                 var fuelLevelValue = results[1][0].data.toFixed(2);
-                console.log(odometerValue, fuelLevelValue);
+                // console.log(odometerValue, fuelLevelValue);
 
                 service.tooltip.show({
                     icon: "https://www.svgimages.com/svg-image/s5/odometer-half-256x256.png",
