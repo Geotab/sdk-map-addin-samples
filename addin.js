1dd9286b2d656815a5eb487293bde5405691bf6a
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2019-08-08 23:48:43 -0700

    Feat: Main Functionality of Addin  - Display odometer & fuel level information for the vehicle you hover  - Odometer is shown in user's metric preferance

---
+++ addin.js
@@ -0,0 +1,73 @@
+geotab.addin.request = (elt, service) => {
+    // code for add-in
+
+    var currUser;
+    var isUIMetric;
+    service.api.getSession().then(sessionInfo => {
+        console.log("SESSION:", sessionInfo);
+        currUser = sessionInfo.userName;
+
+        service.api.call("Get", {
+            typeName: "User",
+            search: {
+                name: currUser
+            }
+        }).then(user => {
+            console.log("USER", user);
+            isUIMetric = user[0].isMetric;
+        });
+    });
+
+    // Subscribe to any mouseover events
+    service.events.attach('over', data => {
+        console.log(data);
+        console.log("Type: ", data.type);
+        if (data.type == "trip") {
+            var tripId = data.entity.id;
+            var deviceId = data.entity.device.id;
+            var hoverTimestamp = data.entity.dateTime;
+            console.log(tripId, deviceId, hoverTimestamp);
+
+            service.api.multiCall([
+                ["Get", {
+                    typeName: "StatusData",
+                    search: {
+                        deviceSearch: {
+                            id: deviceId
+                        },
+                        diagnosticSearch: {
+                            id: "DiagnosticOdometerId"
+                        },
+                        fromDate: hoverTimestamp,
+                        toDate: hoverTimestamp
+                    }
+                }],
+                ["Get", {
+                    typeName: "StatusData",
+                    search: {
+                        deviceSearch: {
+                            id: deviceId
+                        },
+                        diagnosticSearch: {
+                            id: "DiagnosticFuelLevelId"
+                        },
+                        fromDate: hoverTimestamp,
+                        toDate: hoverTimestamp
+                    }
+                }]
+            ]).then(results => {
+                let rawOdo = results[0][0].data;
+
+                var odometerValue = isUIMetric ? Math.round(rawOdo / 1000) : Math.round(rawOdo / 1609.344);
+                var fuelLevelValue = results[1][0].data.toFixed(2);
+                console.log(odometerValue, fuelLevelValue);
+
+                service.tooltip.show({
+                    main: "Testing",
+                    secondary: [`Odometer value: ${odometerValue}`, `Fuel level: ${fuelLevelValue}%`]
+                });
+            });
+
+        }
+    });
+};
