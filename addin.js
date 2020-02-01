a9b3d7183ce38bb656f49412f2697818719e7854
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2020-01-31 21:28:55 -0800

    Feat: Add Support for EV Diagnostics  - Check current battery status  - Checking charge status  - Display additional diagnostic levels  - Display vehicle type (PHEV, HEV, BEV, or ICE) in main tooltip heading

--- addin.js
+++ addin.js
@@ -54,18 +54,73 @@ geotab.addin.request = (elt, service) => {
                         fromDate: hoverTimestamp,
                         toDate: hoverTimestamp
                     }
+                }],
+                ["Get", {
+                    typeName: "StatusData",
+                    search: {
+                        deviceSearch: {
+                            id: deviceId
+                        },
+                        diagnosticSearch: {
+                            id: "atlKz5GniuEOmiSk76mlTdg"
+                        },
+                        fromDate: hoverTimestamp,
+                        observeActiveState: true,
+                    }
+                }],
+                ["Get", {
+                    typeName: "StatusData",
+                    search: {
+                        deviceSearch: {
+                            id: deviceId
+                        },
+                        diagnosticSearch: {
+                            id: "DiagnosticElectricVehicleChargingStateId"
+                        },
+                        fromDate: hoverTimestamp,
+                        observeActiveState: true,
+                    }
                 }]
             ]).then(results => {
                 let rawOdo = results[0][0].data;
 
                 var odometerValue = isUIMetric ? `${Math.round(rawOdo / 1000)} km` : `${Math.round(rawOdo / 1609.344)} mi`;
-                var fuelLevelValue = results[1][0].data.toFixed(2);
+                var fuelLevelValue = results[1][0] ? results[1][0].data.toFixed(2) : null;
                 // console.log(odometerValue, fuelLevelValue);
+
+                var stateOfChargeValue = results[2][0] ? results[2][0].data.toFixed(2) : null;
+                var chargingStateValue = results[3][results[3].length - 1] ? results[3][results[3].length - 1].data : null;
+
+                var tooltipInformation = [`Odometer value: ${odometerValue}`];
+
+                var mainInfo = " ";
+
+                if (fuelLevelValue) {
+                    tooltipInformation.push(`Fuel level: ${fuelLevelValue}%`);
+                }
+
+                if (stateOfChargeValue) {
+                    tooltipInformation.push(`Battery charge level: ${stateOfChargeValue}%`);
+                }
+
+                if (chargingStateValue) {
+                    tooltipInformation.push(`Currently charging? ${chargingStateValue}`);
+                }
+
+                if (stateOfChargeValue && chargingStateValue && fuelLevelValue) {
+                    mainInfo = "PHEV";
+                } else if (stateOfChargeValue && fuelLevelValue && !chargingStateValue) {
+                    mainInfo = "HEV";
+                } else if (stateOfChargeValue && chargingStateValue && !fuelLevelValue) {
+                    mainInfo = "BEV";
+                } else if (!stateOfChargeValue && !chargingStateValue && fuelLevelValue) {
+                    mainInfo = "ICE";
+                }
 
                 service.tooltip.show({
                     icon: "https://www.svgimages.com/svg-image/s5/odometer-half-256x256.png",
-                    main: " ",
-                    secondary: [`Odometer value: ${odometerValue}`, `Fuel level: ${fuelLevelValue}%`]
+                    main: mainInfo,
+                    secondary: tooltipInformation
                 }, 2);
             });
 
