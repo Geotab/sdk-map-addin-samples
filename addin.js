043adfe694dc033be05d5452f8e2d2e8ad505a9a
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2020-10-05 20:52:15 -0700

    Refactor: Clean-up & caching  - Remove vehicle-type determination logic  - Set up a cache in the session storage to store status data by minute    to reduce number of calls being made  - Remove icon.svg  - Update config file for live map

--- addin.js
+++ addin.js
@@ -1,125 +1,143 @@
 geotab.addin.request = (elt, service) => {
     // code for add-in
 
-    var currUser;
-    var isUIMetric;
+    let isUIMetric;
     service.api.getSession().then(sessionInfo => {
-        // console.log("SESSION:", sessionInfo);
-        currUser = sessionInfo.userName;
-
         service.api.call("Get", {
             typeName: "User",
             search: {
-                name: currUser
+                name: sessionInfo.userName
             }
         }).then(user => {
-            // console.log("USER", user);
             isUIMetric = user[0].isMetric;
         });
     });
 
     // Subscribe to any mouseover events
     service.events.attach('over', data => {
-        // console.log(data);
-        // console.log("Type: ", data.type);
         if (data.type == "trip" || data.type == "device") {
-            var tripId = data.entity.id;
-            var deviceId = data.type == "trip" ? data.entity.device.id : data.entity.id;
-            var hoverTimestamp = data.type == "trip" ? data.entity.dateTime : new Date().toISOString();
-            // console.log(tripId, deviceId, hoverTimestamp);
+            let deviceId = data.type == "trip" ? data.entity.device.id : data.entity.id;
+            let hoverTimestamp = data.type == "trip" ? data.entity.dateTime : new Date().toISOString();
 
-            service.api.multiCall([
-                ["Get", {
-                    typeName: "StatusData",
-                    search: {
-                        deviceSearch: {
-                            id: deviceId
-                        },
-                        diagnosticSearch: {
-                            id: "DiagnosticOdometerId"
-                        },
-                        fromDate: hoverTimestamp,
-                        toDate: hoverTimestamp
+            let sessionData = grabSessionData(deviceId, hoverTimestamp);
+
+            if (!sessionData) {
+                service.api.multiCall([
+                    ["Get", {
+                        typeName: "StatusData",
+                        search: {
+                            deviceSearch: {
+                                id: deviceId
+                            },
+                            diagnosticSearch: {
+                                id: "DiagnosticOdometerId"
+                            },
+                            fromDate: hoverTimestamp,
+                            toDate: hoverTimestamp
+                        }
+                    }],
+                    ["Get", {
+                        typeName: "StatusData",
+                        search: {
+                            deviceSearch: {
+                                id: deviceId
+                            },
+                            diagnosticSearch: {
+                                id: "DiagnosticFuelLevelId"
+                            },
+                            fromDate: hoverTimestamp,
+                            toDate: hoverTimestamp
+                        }
+                    }],
+                    ["Get", {
+                        typeName: "StatusData",
+                        search: {
+                            deviceSearch: {
+                                id: deviceId
+                            },
+                            diagnosticSearch: {
+                                id: "atlKz5GniuEOmiSk76mlTdg"
+                            },
+                            fromDate: hoverTimestamp,
+                            observeActiveState: true,
+                        }
+                    }]
+                ]).then(results => {
+                    let rawOdo = results[0][0].data;
+
+                    let odometerValue = isUIMetric ? `${Math.round(rawOdo / 1000)} km` : `${Math.round(rawOdo / 1609.344)} mi`;
+                    let fuelLevelValue = results[1][0] ? results[1][0].data.toFixed(2) : null;
+
+                    let stateOfChargeValue = results[2][0] ? results[2][0].data.toFixed(2) : null;
+
+                    if (data.type !== "device") {
+                        addSessionData(deviceId, hoverTimestamp, rawOdo, fuelLevelValue, stateOfChargeValue);
                     }
-                }],
-                ["Get", {
-                    typeName: "StatusData",
-                    search: {
-                        deviceSearch: {
-                            id: deviceId
-                        },
-                        diagnosticSearch: {
-                            id: "DiagnosticFuelLevelId"
-                        },
-                        fromDate: hoverTimestamp,
-                        toDate: hoverTimestamp
-                    }
-                }],
-                ["Get", {
-                    typeName: "StatusData",
-                    search: {
-                        deviceSearch: {
-                            id: deviceId
-                        },
-                        diagnosticSearch: {
-                            id: "atlKz5GniuEOmiSk76mlTdg"
-                        },
-                        fromDate: hoverTimestamp,
-                        observeActiveState: true,
-                    }
-                }],
-                ["Get", {
-                    typeName: "StatusData",
-                    search: {
-                        deviceSearch: {
-                            id: deviceId
-                        },
-                        diagnosticSearch: {
-                            id: "DiagnosticElectricVehicleChargingStateId"
-                        },
-                        fromDate: hoverTimestamp,
-                        observeActiveState: true,
-                    }
-                }]
-            ]).then(results => {
-                let rawOdo = results[0][0].data;
-
-                var odometerValue = isUIMetric ? `${Math.round(rawOdo / 1000)} km` : `${Math.round(rawOdo / 1609.344)} mi`;
-                var fuelLevelValue = results[1][0] ? results[1][0].data.toFixed(2) : null;
-                // console.log(odometerValue, fuelLevelValue);
-
-                var stateOfChargeValue = results[2][0] ? results[2][0].data.toFixed(2) : null;
-                var chargingStateValue = results[3][results[3].length - 1] ? results[3][results[3].length - 1].data : null;
-
-                var tooltipInformation = [`Odometer value: ${odometerValue}`];
-
-                var mainInfo = " ";
-
-                if (fuelLevelValue) {
-                    tooltipInformation.push(`Fuel level: ${fuelLevelValue}%`);
-                }
-
-                if (stateOfChargeValue) {
-                    tooltipInformation.push(`Battery charge level: ${stateOfChargeValue}%`);
-                }
-
-                if (stateOfChargeValue && chargingStateValue && fuelLevelValue) {
-                    mainInfo = "PHEV";
-                } else if (stateOfChargeValue && fuelLevelValue && !chargingStateValue) {
-                    mainInfo = "HEV";
-                } else if (stateOfChargeValue && chargingStateValue && !fuelLevelValue) {
-                    mainInfo = "BEV";
-                } else if (!stateOfChargeValue && !chargingStateValue && fuelLevelValue) {
-                    mainInfo = "ICE";
-                }
-
-                service.tooltip.show({
-                    icon: "https://www.svgimages.com/svg-image/s5/odometer-half-256x256.png",
-                    main: mainInfo,
-                    secondary: tooltipInformation
-                }, 2);
-            });
-
+                    
+                    setTooltip(odometerValue, fuelLevelValue, stateOfChargeValue);
+                });
+            } else {
+                let odometerValue = isUIMetric ? `${Math.round(sessionData.rawOdometer / 1000)} km` : `${Math.round(sessionData.rawOdometer / 1609.344)} mi`;
+                setTooltip(odometerValue, sessionData.fuelPercentage, sessionData.evChargePercentage);
+            }
         }
     });
+
+    // Look for cache in sessionStorage & retrieve if found
+    let grabSessionData = function (deviceId, time) {
+        let scrubbedTime = new Date(time).setMilliseconds(0)
+        scrubbedTime = new Date(scrubbedTime).toISOString();
+        let cache = JSON.parse(sessionStorage.getItem('odo_fuel_cache'));
+        if (cache) {
+            if (Object.keys(cache).length > 0) {
+                if (cache[deviceId]) {
+                    return cache[deviceId][scrubbedTime] ? cache[deviceId][scrubbedTime] : null;
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            sessionStorage.setItem('odo_fuel_cache', JSON.stringify({}));
+            return null;
+        }
+    };
+
+    // Add data to the cache in sesionStorage
+    let addSessionData = function (deviceId, time, odo, fuel, stateOfChargeValue) {
+        let scrubbedTime = new Date(time).setMilliseconds(0)
+        scrubbedTime = new Date(scrubbedTime).toISOString();
+        let newData = {
+            'rawOdometer': odo,
+            'fuelPercentage': fuel,
+            'evChargePercentage': stateOfChargeValue
+        };
+        
+        let cache = JSON.parse(sessionStorage.getItem('odo_fuel_cache'));
+        if (!cache[deviceId]) {
+            cache[deviceId] = {};
+        }
+        cache[deviceId][scrubbedTime] = newData;
+        sessionStorage.setItem('odo_fuel_cache', JSON.stringify(cache));
+    };
+
+    // Add information to the tooltip
+    let setTooltip = function (odometerValue, fuelLevelValue, stateOfChargeValue) {
+        let tooltipInformation = [`Odometer value: ${odometerValue}`];
+
+        if (fuelLevelValue) {
+            tooltipInformation.push(`Fuel level: ${fuelLevelValue}%`);
+        }
+
+        if (stateOfChargeValue) {
+            tooltipInformation.push(`Battery charge level: ${stateOfChargeValue}%`);
+        }
+
+        service.tooltip.show({
+            icon: "https://www.svgimages.com/svg-image/s5/odometer-half-256x256.png",
+            main: " ",
+            secondary: tooltipInformation
+        }, 2);
+    }
 };
